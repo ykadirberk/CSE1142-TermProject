5e633a44f20970974e4103ba27da312559fb976d
ykadirberk <ykadirberk2@gmail.com>
2021-05-03 13:52:09 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+	}
+
+}
