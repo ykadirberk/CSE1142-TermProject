274ae6d69b0299170f092de11f14acaacc668cc8
ykadirberk <ykadirberk2@gmail.com>
2021-05-06 23:31:08 +0300

    main.class düzeltmeye çalıştım

--- src/GameLayoutDesign.java
+++ src/GameLayoutDesign.java
@@ -1,15 +1,9 @@
 
-import java.awt.Color;
-
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.geometry.Insets;
